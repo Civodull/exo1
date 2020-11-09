5fcb3098fb77a9d9a8ff9d006eff52fc03803c6e
Civodull <olivierndi57@gmail.com>
2020-11-09 14:24:29 +0000

    Exercice deux

---
+++ exo2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+
+int main() {
+   
+   int n,i=1;
+/*
+do
+{
+   printf("Entrer le nombre \n");
+   scanf("%d", &n);
+   i++;
+
+} while (n<1 || n>10);
+*/
+/*
+for (int i = 1; i <=10; i++)
+{
+    printf("Entrer un entier entre 1 et 10 ");
+    scanf("%d", &n);
+    if (n<1 || n>10)
+    {
+        break;
+    }
+    else
+    {
+        printf("Vous avez saisi %d\n",n);
+    }break;
+}*/
+while (n<1 || n>10)
+{
+    printf("Entrer un nombre entre 1 et 10\n");
+    scanf("%d", &n);
+}
+
+    return 0;
+}
